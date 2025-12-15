38cebca71ffc796b11c329dd26b878efdcf6128d
razorbladee2k-svg <razorbladee2k@gmail.com>
2025-12-15 12:41:11 +0800

    Update script.js

--- script.js
+++ script.js
@@ -1,1 +1,20 @@
+function showSection(sectionId) {
+  document.querySelectorAll('.content').forEach(section => {
+    section.classList.add('hidden');
+  });
+
+  document.getElementById(sectionId).classList.remove('hidden');
+}
+
+function checkAnswer(correct) {
+  const result = document.getElementById("result");
+
+  if (correct) {
+    result.textContent = "Correct! Good job 🎉";
+    result.style.color = "green";
+  } else {
+    result.textContent = "Wrong. Try again ❌";
+    result.style.color = "red";
+  }
+}
 
